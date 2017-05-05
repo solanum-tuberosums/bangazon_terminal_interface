d2feb0a0a8671605d2326e55490d8ba24e576f0b
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-05-05 08:39:39 -0500

    removed str return from Product.py

--- src/Product.py
+++ src/Product.py
@@ -8,9 +8,3 @@ class Product():
         self.description = faker.text()
         self.product_type = 1
         self.customer_id = customer_id
-
-    def __str__(self):
-        return "Title: {}, Description: {}, Product Type: {}, Customer ID: {}".format(self.title, self.description, self.product_type, self.customer_id)
-
-product = Product(1)
-print(product)
