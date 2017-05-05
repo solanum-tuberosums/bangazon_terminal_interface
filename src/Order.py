cd9ee7b68907e72d2f0e2abb1d0e92c064431533
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-05-05 08:38:55 -0500

    added functionality to Customer, Order, and PaymentType.py. created Product.py

--- src/Order.py
+++ src/Order.py
@@ -1,5 +1,9 @@
+import faker
+
+faker = faker.Faker()
+
 class Order():
     def __init__(self, customer_id):
     self.payment_type_id = None
-    self.order_date = factory.Faker('date')
+    self.order_date = faker.date()
     self.customer_id = customer_id
