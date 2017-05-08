7406d2a0a9c4f8ca03937de3063c58a019a8f395
Jessica Younker <jessica.younker@gmail.com>
2017-05-08 11:27:38 -0500

    added methods for flushing tables, saving customer, completing order, and getting all

---
+++ src/methods.py
@@ -0,0 +1,11 @@
+def get_all_from_table(table_name):
+    return [(1),(2,'j','b'), (3)]
+
+def complete_order(order_id, pmt_type_id):
+    return (1, 3,'2016-01-21', 5, '2017-01-01')
+    
+def get_active_customer_order(customer_id):
+    return 1
+
+def flush_table(table_name):
+    pass
