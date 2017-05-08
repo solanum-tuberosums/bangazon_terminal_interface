422fd0d4bc25857b7ab75d4c50225c6eaca26f9d
Blaise Roberts <blaiseroberts@Blaises-MacBook-Pro-2.local>
2017-05-08 15:30:24 -0500

    added logic for adding product to order

--- src/main.py
+++ src/main.py
@@ -3,20 +3,21 @@ import datetime
 
 active_customer_id = None
 
-def run_ordering_system():
-    print("*********************************************************")
-    print("**  Welcome to Bangazon! Command Line Ordering System  **")
-    print("*********************************************************")
-    print("1. Create a customer account")
-    print("2. Choose active customer")
-    print("3. Create a payment option")
-    print("4. Add product to shopping cart")
-    print("5. Complete an order")
-    print("6. See product popularity")
-    print("7. Leave Bangazon!")
-
-    menu_command = int(input("Please select the number that corresponds to your menu option\n"))
+def run_ordering_system(menu_command=None):
     global active_customer_id
+
+    if menu_command == None:
+        print("*********************************************************")
+        print("**  Welcome to Bangazon! Command Line Ordering System  **")
+        print("*********************************************************")
+        print("1. Create a customer account")
+        print("2. Choose active customer")
+        print("3. Create a payment option")
+        print("4. Add product to shopping cart")
+        print("5. Complete an order")
+        print("6. See product popularity")
+        print("7. Leave Bangazon!")
+        menu_command = int(input("Please select the number that corresponds to your menu option\n"))
 
     if menu_command == 1:
         print("Enter customer name")
@@ -74,7 +75,31 @@ def run_ordering_system():
             run_ordering_system()
 
     if menu_command == 4:
-        pass    
+        if active_customer_id:
+            try:
+                order_id = get_active_customer_order(active_customer_id)
+            except:
+                order_values = [None, datetime.datetime.now(), active_customer_id, None]
+                order_id = save_to_db("CustomerOrder", order_vales)
+            product_list = get_all_from_table("Product")
+            exit_command = (9999,9999,"Done Adding Products")
+            product_list.append(exit_command)
+            # Print list of all customer
+            for counter, product in enumerate(product_list):
+                print(str(counter+1)+". ", product[2])
+            chosen_product_from_menu = int(input())
+            # Check to see if they exited program
+            if len(product_list) == chosen_product_from_menu:
+                run_ordering_system()
+            else:
+                product_id = product_list[chosen_product_from_menu-1][0]
+                save_to_db("ProductOrder", (order_id, product_id))
+                run_ordering_system(menu_command=4)
+
+        else: 
+            print("Please select an active customer or create a new customer. Press any key to return to main menu")
+            input()
+            run_ordering_system()  
     if menu_command == 5:
         pass    
     if menu_command == 6:
