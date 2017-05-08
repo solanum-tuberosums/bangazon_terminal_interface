d598e63745daa9df0049f1dc8f620ea6134f1712
Zak Spence <zakspence@gmail.com>
2017-05-08 15:29:21 -0500

    rebased branch

--- src/methods.py
+++ src/methods.py
@@ -1,15 +1,30 @@
 def get_all_from_table(table_name):
-    return [(1, 1),(2,'j','b'), (3, 2)]
+    conn = sqlite3.connect('db.sqlite3')
+    c = conn.cursor()
+    ordering = str()
+    if table_name.lower() == 'customer':
+        ordering = 'last_name'
+    elif table_name.lower() == 'product':
+        ordering = 'title'
+    command = 'SELECT * FROM {} ORDER BY {}'.format(table_name, ordering)
+    selection = c.execute(command)
+    conn.commit()
+    conn.close()
+    return selection
 
 def complete_order(order_id, pmt_type_id):
-    return (1, 3,'2016-01-21', 5, '2017-01-01')
-    
+    return (1, 3, '2016-01-21', 5, '2017-01-01')
+
 def get_active_customer_order(customer_id):
     return 1
 
 def flush_table(table_name):
-    pass
+    conn = sqlite3.connect('db.sqlite3')
+    c = conn.cursor()
+    command = "DELETE FROM {}".format(table_name)
+    c.execute(command)
+    conn.commit()
+    conn.close()
 
 def save_to_db(table, values):
     return 1
-
