d075151442defc49ddda3dd05b352c30a41bfe80
William A Sims III <willsims14@gmail.com>
2017-05-10 14:22:10 -0500

    Finished refactoring main

--- src/main.py
+++ src/main.py
@@ -60,19 +60,21 @@ def run_ordering_system(menu_command=None):
             # Print list of all customer
             for counter, customer in enumerate(customer_list):
                 print(str(counter+1)+". ", customer[1], customer[2], customer[3])
-            chosen_customer_from_menu = int(input())
-
+            try:
+                chosen_customer_from_menu = int(input(' > '))
+            except:
+                chosen_customer_from_menu = 0
             # Added this because if user entered 0:
             #   the last index of the list would be set as the active customer
             if chosen_customer_from_menu <= 0:
                 print('\n --- CUSTOMER DOES NOT EXIST ---\n')
-                # menu_command = 2
+                menu_command = 2
             else:
 
                 # Set Active Customer
                 try:
                     active_customer_id = customer_list[chosen_customer_from_menu-1][0]
-                    print("\n --- NEW ACTIVE CUSTOMER: ", customer_list[chosen_customer_from_menu-1][1], customer_list[chosen_customer_from_menu-1][3] + " ---\n")
+                    print("\n *** NEW ACTIVE CUSTOMER: ", customer_list[chosen_customer_from_menu-1][1], customer_list[chosen_customer_from_menu-1][3] + " ***\n")
                     menu_command = None
                 except:
                     print("\n -- CUSTOMER DOES NOT EXIST ---\n")
@@ -81,16 +83,20 @@ def run_ordering_system(menu_command=None):
             if active_customer_id:
                 print("Name this payment account")
                 account_label = input(' > ')
-                print("Enter payment type (e.g. AmEx, Visa, Checking)")
-                account_type = input(' > ')
-                print("Enter account number")
-                account_number = input(' > ')
-                customer_id = active_customer_id
-                payment_type_values = list()
-                payment_type_values.extend([account_label, account_type, account_number, customer_id])
-                save_to_db("PaymentType", payment_type_values)
-                print("\n --- PAYMENT TYPE SAVED --- \n")
-                menu_command = None
+                if len(account_label) <= 0:
+                    print("\n --- PLEASE GIVE YOUR PAYMENT TYPE A NAME ---\n")
+                    menu_command = 3
+                else:
+                    print("Enter payment type (e.g. AmEx, Visa, Checking)")
+                    account_type = input(' > ')
+                    print("Enter account number")
+                    account_number = input(' > ')
+                    customer_id = active_customer_id
+                    payment_type_values = list()
+                    payment_type_values.extend([account_label, account_type, account_number, customer_id])
+                    save_to_db("PaymentType", payment_type_values)
+                    print("\n *** PAYMENT TYPE SAVED *** \n")
+                    menu_command = None
             else:
                 input("Please select an active customer or create a new customer. Press enter to return to main menu.\n > ")
                 menu_command = None
@@ -126,6 +132,7 @@ def run_ordering_system(menu_command=None):
                         try:
                             product_id = product_list[chosen_product_from_menu-1][0]
                             save_to_db("ProductOrder", (product_id, order_id))
+                            print("\n *** PRODUCT ADDED ***\n")
                             menu_command = 4
                         except:
                             print("\n --- PRODUCT DOES NOT EXIST ---\n")
@@ -150,8 +157,8 @@ def run_ordering_system(menu_command=None):
                 # Check order total
                 if order_total != None:
                     rounded_order_total = round(order_total, 2)
-                    print("Your order total is ${}. Ready to purchase?".format(rounded_order_total))
-                    response = input("Y/N\t")
+                    print("Your order total is ${}. Ready to purchase? (Y/N)".format(rounded_order_total))
+                    response = input(" > ")
                     if response.lower() == "y":
                         # Get payment types for customer
                         payment_type_list = get_all_from_table("PaymentType", customer_id=active_customer_id)
@@ -160,11 +167,11 @@ def run_ordering_system(menu_command=None):
                             for counter, payment in enumerate(payment_type_list):
                                 print(str(counter+1)+". ", payment[1], payment[2])
                             try:
-                                chosen_payment_type = int(input())
+                                chosen_payment_type = int(input(' > '))
                                 chosen_payment_type_id = payment_type_list[chosen_payment_type-1][0]
                                 # Update order with chosen payment type id and date paid
                                 complete_order(order_id, chosen_payment_type_id)
-                                input("Your order is complete! Press enter to return to main menu.\n > ")
+                                input("Your order is complete! Press enter to return to main menu.\n")
                                 menu_command = None
                             except:
                                 print("\n --- MUST ENTER A SHOWN INTEGER ---\n")
@@ -195,78 +202,78 @@ def run_ordering_system(menu_command=None):
             # Get list of tuples for the popular products
             popular_product_list = get_popular_products()
 
-        if len(popular_product_list)>0:
+            if len(popular_product_list)>0:
 
-            # Set the max column width
-            product_column_total_spaces = 18
-            order_column_total_spaces = 11
-            customer_column_total_spaces = 11
-            revenue_column_total_spaces = 14
+                # Set the max column width
+                product_column_total_spaces = 18
+                order_column_total_spaces = 11
+                customer_column_total_spaces = 11
+                revenue_column_total_spaces = 14
 
-            # Set up Table
-            print("Product           Orders     Customers  Revenue")
-            print("*******************************************************")
-
-
-            #Loop Through list of tuples to print each row in table
-            for product in popular_product_list:
-                #Add columns for use in table totals
-                total_orders += product[1]
-                total_customers += product[2]
-                total_revenue += product[3]
-
-                
-                # Convert Tuple to list and truncate longer values in each column
-                product = list(product)
-                product[3] = round(product[3], 2)
-                if len(str(product[0]))>14:
-                    product[0] = str(product[0])[:14]+"..." 
-                if len(str(product[1]))>7:
-                    product[1] = str(product[1])[:7]+"..."
-                if len(str(product[2]))>7:
-                    product[2] = str(product[2])[:7]+"..."
-                if len(str(product[3]))>10:
-                    product[3] = str(product[3])[:10]+"..."
-
-                # Calcualte spaces for each column in each row in table
-                space = " "
-                product_column_spaces = product_column_total_spaces-len(str(product[0]))
-                order_column_spaces = order_column_total_spaces-len(str(product[1]))
-                customer_column_spaces = customer_column_total_spaces-len(str(product[2]))
-
-                # Print each row in table
-                print(str(product[0])+(space*product_column_spaces)+str(product[1])+(space*order_column_spaces)+str(product[2])+(space*customer_column_spaces)+"$"+str(product[3]))
-
-            print("*******************************************************")
-
-            rounded_total_revenue = round(total_revenue, 2)
+                # Set up Table
+                print("Product           Orders     Customers  Revenue")
+                print("*******************************************************")
 
 
-            # Truncate values to fit into columns
-            if len(str(total_orders))>7:
-                total_orders = str(total_orders)[:7]+"..."
-            if len(str(total_customers))>7:
-                total_customers = str(total_customers)[:7]+"..."
-            if len(str(total_revenue))>10:
-                total_revenue = str(total_revenue)[:10]+"..."
+                #Loop Through list of tuples to print each row in table
+                for product in popular_product_list:
+                    #Add columns for use in table totals
+                    total_orders += product[1]
+                    total_customers += product[2]
+                    total_revenue += product[3]
 
-            # Calculate the number of spaces needed for each column
-            total_orders_spaces = order_column_total_spaces-len(str(total_orders))
-            total_customers_spaces = customer_column_total_spaces-len(str(total_customers))
-            total_revenue_spaces = revenue_column_total_spaces-len(str(total_revenue))
 
-            # Print totals in their perfectly sized columns
+                    # Convert Tuple to list and truncate longer values in each column
+                    product = list(product)
+                    product[3] = round(product[3], 2)
+                    if len(str(product[0]))>14:
+                        product[0] = str(product[0])[:14]+"..."
+                    if len(str(product[1]))>7:
+                        product[1] = str(product[1])[:7]+"..."
+                    if len(str(product[2]))>7:
+                        product[2] = str(product[2])[:7]+"..."
+                    if len(str(product[3]))>10:
+                        product[3] = str(product[3])[:10]+"..."
 
-            print("Totals:           "+str(total_orders)+(space*total_orders_spaces)+str(total_customers)+(space*total_customers_spaces)+"$"+str(rounded_total_revenue))
-            input("Press enter to return to the main menu.\n")
-            run_ordering_system()
-        else:
-            input("Nothing has been purchased yet, there's no contest broseph.\nPress enter to return to the main menu\n")
-            run_ordering_system()
-            
+                    # Calcualte spaces for each column in each row in table
+                    space = " "
+                    product_column_spaces = product_column_total_spaces-len(str(product[0]))
+                    order_column_spaces = order_column_total_spaces-len(str(product[1]))
+                    customer_column_spaces = customer_column_total_spaces-len(str(product[2]))
+
+                    # Print each row in table
+                    print(str(product[0])+(space*product_column_spaces)+str(product[1])+(space*order_column_spaces)+str(product[2])+(space*customer_column_spaces)+"$"+str(product[3]))
+
+                print("*******************************************************")
+
+                rounded_total_revenue = round(total_revenue, 2)
+
+
+                # Truncate values to fit into columns
+                if len(str(total_orders))>7:
+                    total_orders = str(total_orders)[:7]+"..."
+                if len(str(total_customers))>7:
+                    total_customers = str(total_customers)[:7]+"..."
+                if len(str(total_revenue))>10:
+                    total_revenue = str(total_revenue)[:10]+"..."
+
+                # Calculate the number of spaces needed for each column
+                total_orders_spaces = order_column_total_spaces-len(str(total_orders))
+                total_customers_spaces = customer_column_total_spaces-len(str(total_customers))
+                total_revenue_spaces = revenue_column_total_spaces-len(str(total_revenue))
+
+                # Print totals in their perfectly sized columns
+
+                print("Totals:           "+str(total_orders)+(space*total_orders_spaces)+str(total_customers)+(space*total_customers_spaces)+"$"+str(rounded_total_revenue))
+                input("Press enter to return to the main menu.\n")
+                menu_command = None
+            else:
+                input("Nothing has been purchased yet, there's no contest broseph.\nPress enter to return to the main menu\n")
+                menu_command = None
+
     if menu_command == 7:
         print("Cya, Sucka! Thanks for visiting Bangazon.")
-        pass
+
 
 
 
